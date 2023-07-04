f575682d5e6ee030d3038af9fd510c59acbdc307
SahreyarArafat <132135025+SofolotarDak@users.noreply.github.com>
2023-07-05 02:33:18 +0600

    Update multer-upload.util.js

--- utils/multer-upload.util.js
+++ utils/multer-upload.util.js
@@ -2,50 +2,44 @@
 const multer = require('multer');
 const path = require('path');
 
-// renaming the uploaded file and defind it's storage
+// Renaming the uploaded file and defining its storage
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'public/Uploads');
     },
-    fileFilter: (req, file, cb) => {
-        if (file.fieldname === 'storyimage') {
-            try {
-                const fileExt = path.extname(file.originalname);
-                const allowedExtensions = ['.png', '.jpg'];
-                const maxSize = 1 * 1024 * 1024; // 1MB
-
-                if (
-                    allowedExtensions.includes(fileExt) &&
-                    file.size <= maxSize
-                ) {
-                    cb(null, true);
-                } else {
-                    throw new Error(
-                        'Invalid file. Only PNG and JPG files up to 1MB are allowed.'
-                    );
-                }
-            } catch (error) {
-                cb(error);
-            }
+    filename: (req, file, cb) => {
+        try {
+            const fileExt = path.extname(file.originalname);
+            const fileName = `${file.originalname
+                .replace(fileExt, '')
+                .toLowerCase()
+                .split(' ')
+                .join('-')}-${Date.now()}`;
+            cb(null, fileName + fileExt);
+        } catch (error) {
+            cb(error);
         }
     },
 });
 
-// prepare the multer upload object
+// Prepare the multer upload object
 const upload = multer({
     storage,
     limits: {
         fileSize: 1000000, // 1MB
     },
     fileFilter: (req, file, cb) => {
-        if (file.fieldname === 'storyimage') {
-            const fileExt = path.extname(file.originalname);
-
-            if (fileExt === '.png' || fileExt === '.jpg') {
-                cb(null, true);
-            } else {
-                cb(null, false);
+        try {
+            if (file.fieldname === 'storyimage') {
+                const fileExt = path.extname(file.originalname);
+                if (fileExt === '.png' || fileExt === '.jpg') {
+                    cb(null, true);
+                } else {
+                    cb(null, false);
+                }
             }
+        } catch (error) {
+            cb(error);
         }
     },
 });
